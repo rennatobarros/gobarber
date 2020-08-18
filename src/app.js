c1dcfbe5d266d88a04fc4bffb14e791f5d721e8c
Renato Barros <rennatobarros@gmail.com>
2020-08-18 00:49:23 -0300

    Eslint, Prettier and Editorconfig setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import express from "express";
-import routes  from './routes';
+import express from 'express';
+
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
-    
     this.middlewares();
     this.routes();
   }
